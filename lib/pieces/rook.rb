0c5dd4344c1ea163a3380d11af03298b79b70656
Ben <ben.d.godfrey2002@gmail.com>
2024-08-31 15:07:53 +0100

    test: add tests for piece interactions

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -3,6 +3,8 @@ require_relative("piece")
 class Rook < Piece
   def initialize(colour)
     @colour = colour
-    @moves = {once: [], repeats: []}
+    @moves = {once: [], repeats: [[1,0], [0,1]]}
   end
+
+  attr_accessor :colour
 end
--- spec/moves_spec.rb
+++ spec/moves_spec.rb
@@ -10,13 +10,12 @@ require_relative '../lib/pieces/queen'
 require_relative '../lib/pieces/king'
 
 RSpec.describe 'Piece Movement' do
-  let(:board) { Board.new }
+  let(:board) { Board.new(true) }
 
   describe 'Pawn movement' do
     it 'allows pawns to move forward by one square' do
       pawn = Pawn.new("white")
       board.place_piece(pawn, [1, 0]) # Place white pawn on a2 (index [1, 0])
-      p(pawn.valid_moves([1,0], board))
       expect(pawn.valid_move?([1, 0], [2, 0], board)).to be_truthy # a2 to a3
     end
 
@@ -39,25 +38,57 @@ RSpec.describe 'Piece Movement' do
       board.place_piece(enemy_pawn, [3, 1]) # Place black pawn on b4
       expect(pawn.valid_move?([2, 0], [3, 1], board)).to be_truthy # a3 to b4
     end
+
+    it 'does not allow pawns to capture their own color pieces' do
+      pawn = Pawn.new("white")
+      ally_pawn = Pawn.new("white")
+      board.place_piece(pawn, [2, 0]) # Place white pawn on a3
+      board.place_piece(ally_pawn, [3, 1]) # Place white pawn on b4
+      expect(pawn.valid_move?([2, 0], [3, 1], board)).to be_falsey # a3 to b4 (should not capture own color)
+    end
   end
 
   describe 'Rook movement' do
-    xit 'allows rooks to move vertically' do
+    it 'allows rooks to move vertically' do
       rook = Rook.new("white")
       board.place_piece(rook, [0, 0]) # Place white rook on a1
       expect(rook.valid_move?([0, 0], [3, 0], board)).to be_truthy # a1 to a4
     end
 
-    xit 'allows rooks to move horizontally' do
+    it 'allows rooks to move horizontally' do
       rook = Rook.new("white")
       board.place_piece(rook, [0, 0]) # Place white rook on a1
       expect(rook.valid_move?([0, 0], [0, 4], board)).to be_truthy # a1 to e1
     end
 
-    xit 'does not allow rooks to move diagonally' do
+    it 'does not allow rooks to move diagonally' do
       rook = Rook.new("white")
       board.place_piece(rook, [0, 0]) # Place white rook on a1
       expect(rook.valid_move?([0, 0], [2, 2], board)).to be_falsey # a1 to c3 (invalid)
+    end
+
+    it 'does not allow rooks to jump over pieces' do
+      rook = Rook.new("white")
+      blocking_piece = Pawn.new("black")
+      board.place_piece(rook, [0, 0]) # Place white rook on a1
+      board.place_piece(blocking_piece, [1, 0]) # Place black pawn on a2
+      expect(rook.valid_move?([0, 0], [2, 0], board)).to be_falsey # a1 to a3 (blocked by pawn)
+    end
+
+    it 'allows rooks to capture opponent pieces' do
+      rook = Rook.new("white")
+      opponent_piece = Pawn.new("black")
+      board.place_piece(rook, [0, 0]) # Place white rook on a1
+      board.place_piece(opponent_piece, [0, 3]) # Place black pawn on d1
+      expect(rook.valid_move?([0, 0], [0, 3], board)).to be_truthy # a1 to d1 (captures black pawn)
+    end
+
+    it 'does not allow rooks to capture their own color pieces' do
+      rook = Rook.new("white")
+      ally_piece = Pawn.new("white")
+      board.place_piece(rook, [0, 0]) # Place white rook on a1
+      board.place_piece(ally_piece, [0, 3]) # Place white pawn on d1
+      expect(rook.valid_move?([0, 0], [0, 3], board)).to be_falsey # a1 to d1 (should not capture own color)
     end
   end
 
@@ -69,10 +100,28 @@ RSpec.describe 'Piece Movement' do
       expect(knight.valid_move?([0, 1], [1, 3], board)).to be_truthy # b1 to d2
     end
 
-    xit 'does not allow knights to move like a bishop' do
+    xit 'does not allow knights to jump over pieces' do
       knight = Knight.new("white")
+      blocking_piece = Pawn.new("black")
       board.place_piece(knight, [0, 1]) # Place white knight on b1
-      expect(knight.valid_move?([0, 1], [2, 3], board)).to be_falsey # b1 to d3 (invalid)
+      board.place_piece(blocking_piece, [1, 3]) # Place black pawn on c3
+      expect(knight.valid_move?([0, 1], [2, 2], board)).to be_truthy # b1 to c3 (knight should jump over the piece)
+    end
+
+    xit 'allows knights to capture opponent pieces' do
+      knight = Knight.new("white")
+      opponent_piece = Pawn.new("black")
+      board.place_piece(knight, [0, 1]) # Place white knight on b1
+      board.place_piece(opponent_piece, [2, 2]) # Place black pawn on c3
+      expect(knight.valid_move?([0, 1], [2, 2], board)).to be_truthy # b1 to c3 (captures black pawn)
+    end
+
+    xit 'does not allow knights to capture their own color pieces' do
+      knight = Knight.new("white")
+      ally_piece = Pawn.new("white")
+      board.place_piece(knight, [0, 1]) # Place white knight on b1
+      board.place_piece(ally_piece, [2, 2]) # Place white pawn on c3
+      expect(knight.valid_move?([0, 1], [2, 2], board)).to be_falsey # b1 to c3 (should not capture own color)
     end
   end
 
@@ -89,6 +138,30 @@ RSpec.describe 'Piece Movement' do
       expect(bishop.valid_move?([0, 2], [0, 3], board)).to be_falsey # c1 to d1 (invalid)
       expect(bishop.valid_move?([0, 2], [1, 2], board)).to be_falsey # c1 to c2 (invalid)
     end
+
+    xit 'does not allow bishops to jump over pieces' do
+      bishop = Bishop.new("white")
+      blocking_piece = Pawn.new("black")
+      board.place_piece(bishop, [0, 2]) # Place white bishop on c1
+      board.place_piece(blocking_piece, [1, 3]) # Place black pawn on d2
+      expect(bishop.valid_move?([0, 2], [2, 4], board)).to be_falsey # c1 to e3 (blocked by pawn)
+    end
+
+    xit 'allows bishops to capture opponent pieces' do
+      bishop = Bishop.new("white")
+      opponent_piece = Pawn.new("black")
+      board.place_piece(bishop, [0, 2]) # Place white bishop on c1
+      board.place_piece(opponent_piece, [2, 4]) # Place black pawn on e3
+      expect(bishop.valid_move?([0, 2], [2, 4], board)).to be_truthy # c1 to e3 (captures black pawn)
+    end
+
+    xit 'does not allow bishops to capture their own color pieces' do
+      bishop = Bishop.new("white")
+      ally_piece = Pawn.new("white")
+      board.place_piece(bishop, [0, 2]) # Place white bishop on c1
+      board.place_piece(ally_piece, [2, 4]) # Place white pawn on e3
+      expect(bishop.valid_move?([0, 2], [2, 4], board)).to be_falsey # c1 to e3 (should not capture own color)
+    end
   end
 
   describe 'Queen movement' do
@@ -104,6 +177,30 @@ RSpec.describe 'Piece Movement' do
       queen = Queen.new("white")
       board.place_piece(queen, [0, 3]) # Place white queen on d1
       expect(queen.valid_move?([0, 3], [1, 5], board)).to be_falsey # d1 to f2 (invalid)
+    end
+
+    xit 'does not allow queens to jump over pieces' do
+      queen = Queen.new("white")
+      blocking_piece = Pawn.new("black")
+      board.place_piece(queen, [0, 3]) # Place white queen on d1
+      board.place_piece(blocking_piece, [1, 3]) # Place black pawn on d2
+      expect(queen.valid_move?([0, 3], [2, 3], board)).to be_falsey # d1 to d3 (blocked by pawn)
+    end
+
+    xit 'allows queens to capture opponent pieces' do
+      queen = Queen.new("white")
+      opponent_piece = Pawn.new("black")
+      board.place_piece(queen, [0, 3]) # Place white queen on d1
+      board.place_piece(opponent_piece, [2, 5]) # Place black pawn on f3
+      expect(queen.valid_move?([0, 3], [2, 5], board)).to be_truthy # d1 to f3 (captures black pawn)
+    end
+
+    xit 'does not allow queens to capture their own color pieces' do
+      queen = Queen.new("white")
+      ally_piece = Pawn.new("white")
+      board.place_piece(queen, [0, 3]) # Place white queen on d1
+      board.place_piece(ally_piece, [2, 5]) # Place white pawn on f3
+      expect(queen.valid_move?([0, 3], [2, 5], board)).to be_falsey # d1 to f3 (should not capture own color)
     end
   end
 
@@ -122,13 +219,28 @@ RSpec.describe 'Piece Movement' do
       expect(king.valid_move?([0, 4], [2, 4], board)).to be_falsey # e1 to e3 (invalid)
     end
 
-    xit 'allows castling under correct conditions' do
+    xit 'does not allow kings to jump over pieces' do
       king = King.new("white")
-      rook = Rook.new("white")
+      blocking_piece = Pawn.new("black")
       board.place_piece(king, [0, 4]) # Place white king on e1
-      board.place_piece(rook, [0, 7]) # Place white rook on h1
-      board.clear_path([0, 4], [0, 7]) # Simulate the path is clear for castling
-      expect(king.valid_move?([0, 4], [0, 6], board)).to be_truthy # e1 to g1 (castling)
+      board.place_piece(blocking_piece, [1, 4]) # Place black pawn on e2
+      expect(king.valid_move?([0, 4], [1, 4], board)).to be_falsey # e1 to e2 (blocked by pawn)
+    end
+
+    xit 'allows kings to capture opponent pieces' do
+      king = King.new("white")
+      opponent_piece = Pawn.new("black")
+      board.place_piece(king, [0, 4]) # Place white king on e1
+      board.place_piece(opponent_piece, [1, 4]) # Place black pawn on e2
+      expect(king.valid_move?([0, 4], [1, 4], board)).to be_truthy # e1 to e2 (captures black pawn)
+    end
+
+    xit 'does not allow kings to capture their own color pieces' do
+      king = King.new("white")
+      ally_piece = Pawn.new("white")
+      board.place_piece(king, [0, 4]) # Place white king on e1
+      board.place_piece(ally_piece, [1, 4]) # Place white pawn on e2
+      expect(king.valid_move?([0, 4], [1, 4], board)).to be_falsey # e1 to e2 (should not capture own color)
     end
   end
 end
